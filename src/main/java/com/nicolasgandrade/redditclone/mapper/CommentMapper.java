a8cf33306867191948569d2919debc0beea338f4
nicolasgandrade <nicolasguerrero72@gmail.com>
2022-01-20 16:29:51 -0300

    feat[comment]: add mapping to comments

---
+++ src/main/java/com/nicolasgandrade/redditclone/mapper/CommentMapper.java
@@ -0,0 +1,22 @@
+package com.nicolasgandrade.redditclone.mapper;
+
+import com.nicolasgandrade.redditclone.dto.CommentDto;
+import com.nicolasgandrade.redditclone.model.Comment;
+import com.nicolasgandrade.redditclone.model.Post;
+import com.nicolasgandrade.redditclone.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "text", source = "commentDto.text")
+    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "post", source = "post")
+    Comment map(CommentDto commentDto, Post post, User user);
+
+    @Mapping(target = "postId", expression = "java(comment.getPost().getPostId())")
+    @Mapping(target = "userName", expression = "java(comment.getUser().getUsername())")
+    CommentDto mapToDto(Comment comment);
+}
